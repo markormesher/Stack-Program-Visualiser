37475850b05930a84056718a934f7285ec67ff23
Mark Ormesher <me@markormesher.co.uk>
2015-01-30 01:58:39 +0000

    Remove dummy data

--- src/stackprogramvisualiser/StackProgramVisualiser.java
+++ src/stackprogramvisualiser/StackProgramVisualiser.java
@@ -2,8 +2,6 @@ package stackprogramvisualiser;
 
 import stackprogramvisualiser.gui.Gui;
 
-import java.util.Stack;
-
 public class StackProgramVisualiser {
 
 	public static void main(String[] args) {
@@ -15,17 +13,11 @@ public class StackProgramVisualiser {
 
 	// let's get the party started
 	public void init() {
-		// dummy data
-		Stack<Integer> dd = new Stack<Integer>();
-		dd.push(12);
-		dd.push(5);
-		dd.push(19);
-		dd.push(0);
-
 		Gui gui = new Gui(this);
 		gui.build();
-		gui.setProgramCounter(13);
-		gui.setStackDataSource(dd);
+
+		// default RHS view
+		gui.setProgramCounter(0);
 		gui.redrawStackGui();
 	}
 
